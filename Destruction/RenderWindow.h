d9efd56e0b9c2ee931e24e1662a392ddef109fcd
jonathanforhan <99012095+jonathanforhan@users.noreply.github.com>
2022-11-20 17:52:50 -0500

    Add files via upload

---
+++ Destruction/RenderWindow.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include<SDL.h>
+
+#include"Game/Entity.h"
+
+#define WIN_WIDTH 1200
+#define WIN_HEIGHT 800
+
+class RenderWindow
+{
+public:
+	RenderWindow();
+	RenderWindow(const char* title, int width, int height);
+	~RenderWindow();
+
+	// Render Loop
+	void Clear();
+	void Render(Entity& entity);
+	void Display();
+
+	// Getters
+	SDL_Window* GetWindow();
+	SDL_Renderer* GetRenderer();
+
+private:
+
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+};
+
---
+++ Destruction/main.cpp
@@ -0,0 +1,9 @@
+#include"Game/Game.h"
+
+int main(int argc, char* argv[])
+{
+	Game Destruction;
+	Destruction.Run();
+
+	return 0;
+}
